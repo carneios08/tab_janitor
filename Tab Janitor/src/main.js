fcac4b2a2d9fb4363024e7c9442f318c95b11f61
Scott Swanson <sargeantmarshie@gmail.com>
2019-06-25 16:34:31 -0500

    Code cleanup
    Still an unstable commit. Need to find a good event to hook into.

--- Tab Janitor/src/main.js
+++ Tab Janitor/src/main.js
@@ -1,54 +1,48 @@
-chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab) {
-    if (tab.url && tab.url !== chrome.tabs.TAB_ID_NONE) {
-        main(tab);
+let entireURL = undefined;
+let currentTab = undefined;
+
+chrome.webNavigation.onCommitted.addListener(function(details) {
+    if (details && details.tabId && details.transitionType === "typed") {
+        chrome.storage.sync.get(['entireURL'], setMode);
+
+        chrome.tabs.get(details.tabId, function(tab) {
+            currentTab = tab;
+        });
+
+        if (currentTab && currentTab.id) {
+            main();
+        }
     }
 });
 
-function main(currentTab) {
-    if (currentTab) {
-        let relatedTabs = getAllTabs(chrome.storage.sync.get(['entireURL'], setMode), currentTab);
-        let relatedTabIDs = [];
-    
-        for (let i = 0; i < relatedTabs.length; i++) {
-            // Do not add the tab to the temp collection if the tab ID DNE.
-            if (relatedTabs[i].id && relatedTabs[i].id !== chrome.tabs.TAB_ID_NONE) {
-                // Do not allow the removal of the current tab if it is included in the collection.
-                if (currentTab.id && currentTab.id !== relatedTabs[i].id) {
-                    relatedTabIDs.push(relatedTabs[i].id);
-                }
-            }
-        }
-    
-        // Remove all of the other related tabs.
-        chrome.tabs.remove(relatedTabIDs);
-    }
-}
-
 function setMode(result) {
-    return result.entireURL;
-}
-
-function getAllTabs(entireURL, currentTabIn) {
-    let tabs = null;
-
-    if (!entireURL) {
-        let baseURL = currentTabIn.url.split('?')[0];
-
-        tabs = chrome.tabs.query({
-            url: baseURL
-        },
-        getAllTabsCallback());
+    if (result && result.entireURL) {
+        entireURL = result.entireURL;
     }
     else {
-        tabs = chrome.tabs.query({
-            url: currentTabIn.url
-        },
-        getAllTabsCallback());
+        entireURL = false;
     }
-
-    return tabs;
 }
 
-function getAllTabsCallback(tabsResult) {
-    return tabsResult;
+function main() {
+    if (!entireURL) {
+        chrome.tabs.query({
+            discarded: false,
+            url: currentTab.url.split('?')[0] + "*"
+        },
+        removeSimilarTabs);
+    }
+    else {
+
+    }
+}
+
+function removeSimilarTabs(resultSet) {
+    if (resultSet) {
+        for (let i = 0; i < resultSet.length; i++) {
+            if (resultSet[i] && currentTab && resultSet[i].id && currentTab.id && resultSet[i].id !== currentTab.id) {
+                chrome.tabs.remove(resultSet[i].id);
+            }
+        }
+    }
 }
